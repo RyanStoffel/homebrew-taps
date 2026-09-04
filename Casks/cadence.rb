a03c290993894ab68102f470e40cb16ecbc3e27e
Ryan Stoffel <ryan@stoffel.org>
2026-09-04 12:53:21 -0700

    Bump cadence to 1.1.0

--- Casks/cadence.rb
+++ Casks/cadence.rb
@@ -1,6 +1,6 @@
 cask "cadence" do
-  version "1.0.0"
-  sha256 "da63f01d349e306a71a6a941eb7a2e6b3bff46100dfd01e8bab537ece0fba7b6"
+  version "1.1.0"
+  sha256 "e5b81ace83e6fe277a325642eea95877d50c3f8ec89ad4bb323de2708c70aedb"
 
   url "https://github.com/RyanStoffel/cadence/releases/download/v#{version}/Cadence.zip"
   name "Cadence"
